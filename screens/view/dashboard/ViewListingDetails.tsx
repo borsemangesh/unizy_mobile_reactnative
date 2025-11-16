abaa25d712030da19323a614d2c3da14b86562c9
borsemangesh <m.borse@incrustsoftware.com>
2025-11-16 11:48:14 +0530

    .

--- screens/view/dashboard/ViewListingDetails.tsx
+++ screens/view/dashboard/ViewListingDetails.tsx
@@ -1054,15 +1054,7 @@ import {
     
     },
     cardconstinerdivider: {
-      // display: 'flex',
-      // flexDirection: 'row',
-      // justifyContent: 'space-between',
-      // alignItems: 'center',
-      // width: '100%',
-      // height: 1.5,
-      // borderStyle: 'dashed',
-      // borderBottomWidth: 1,
-      // borderBottomColor: '#4169B8',
+     
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'space-between',
