8e719e7aad76ec488b5ed817a6fd98c094764d8b
RahulGote2025 <rahul@incrustsoftware.com>
2025-11-19 19:38:42 +0530

    New App Messages Added

--- screens/utils/Constant.tsx
+++ screens/utils/Constant.tsx
@@ -10,10 +10,10 @@ export const Constant = {
   INVALID_USER_DATA_RECEIVED: 'Invalid user data received',
   PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS:'Password must be at least 8 characters long.',
   PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
-  FAIL_TO_SEND_OTP: 'Failed to send OTP',
+  FAIL_TO_SEND_OTP: 'Failed to send OTP. Please try again',
   PLEASE_ENTER_ALL_4_DIGITS_OF_THE_OTP: 'Please enter all 4 digits of the OTP',
-  OTP_ID_MISSING:"OTP ID missing. Please request OTP again.",
-  OPT_VERIFICATION_FAILED:'OTP verification failed',
+  OTP_ID_MISSING:"OTP ID is missing. Please request a new OTP",
+  OPT_VERIFICATION_FAILED:'OTP verification failed. Please try again',
   ALERT_MESSAGE_PLEASE_SELECT_AN_IMAGE_FIRST:'Please select an image first',
   // PUBLIC_KEY:'pk_test_51SIt8yJfHNIMSZxgncWbKpvnGsppBiwxAA3Fk3XleikVjZiMPyTt5lCOAUQCwrbv0HX1fDgJsmHYkfJPRK8kRSOF00JGovG410'
   PUBLIC_KEY:'pk_test_51SIt9CJDRdvrnoGE1i7V1uIAllKH7k2IcrfgNVUJogzXaJRRDoFDHOdRUThOxxLmK0EX8iGOCx7aBcXkWJ1UwsVJ00wqAJWmKy'
