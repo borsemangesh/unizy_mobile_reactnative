296a3eea2b054a2a22792715d0e74fe2d36a2c9c
saurabh <saurabhk@incrustsoftware.com>
2025-11-19 15:25:06 +0530

    pushed  merged code

--- screens/view/dashboard/UserListing.tsx
+++ screens/view/dashboard/UserListing.tsx
@@ -225,6 +225,7 @@ const displayListOfProduct = async (categoryId: number | null, pageNum: number) 
   try {
     const pagesize = 10;
     let url = `${MAIN_URL.baseUrl}user/user-listing?page=${pageNum}&pagesize=${pagesize}&user_id=${members.id}`;
+    let url = `${MAIN_URL.baseUrl}user/user-listing?page=${pageNum}&pagesize=${pagesize}&user_id=${members.id}`;
     //let url = `${MAIN_URL.baseUrl}user/user-listing?user_id=${members.id}`;
     
     if (categoryId) {
