6924d644c02c1d37e3707f51953ce319c1b57eca
saurabh <saurabhk@incrustsoftware.com>
2025-11-22 03:03:14 +0530

    constant file pushed

--- screens/utils/Constant.tsx
+++ screens/utils/Constant.tsx
@@ -2,7 +2,7 @@ export const Constant = {
   REQUIRED_ALL_FIELDS: 'Please fill all required fields',
   VALID_EMAI_LADDRESS: 'Please enter a valid email address',
   PASSWORD_RESET_LINK_SENT: 'Password reset link sent',
-  SOMTHING_WENT_WRONG: 'Something went wrong',
+  SOMTHING_WENT_WRONG: 'Something went wrong.Please try again',
   NETWORK_ERROR_PLEASE_TRY_AGAIN: 'Network error, please try again',
   INVALID_SERVER_RESPONSE: 'Invalid server response',
   INVALID_EMAIL_OR_PASSWORD: 'Invalid Email or Password',
@@ -10,10 +10,10 @@ export const Constant = {
   INVALID_USER_DATA_RECEIVED: 'Invalid user data received',
   PASSWORD_MUST_BE_AT_LEAST_8_CHARACTERS:'Password must be at least 8 characters long.',
   PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
-  FAIL_TO_SEND_OTP: 'Failed to send OTP. Please try again',
+  FAIL_TO_SEND_OTP: 'Failed to send OTP',
   PLEASE_ENTER_ALL_4_DIGITS_OF_THE_OTP: 'Please enter all 4 digits of the OTP',
-  OTP_ID_MISSING:"OTP ID is missing. Please request a new OTP",
-  OPT_VERIFICATION_FAILED:'OTP verification failed. Please try again',
+  OTP_ID_MISSING:"OTP ID missing. Please request new OTP.",
+  OPT_VERIFICATION_FAILED:'OTP verification failed',
   ALERT_MESSAGE_PLEASE_SELECT_AN_IMAGE_FIRST:'Please select an image first',
   // PUBLIC_KEY:'pk_test_51SIt8yJfHNIMSZxgncWbKpvnGsppBiwxAA3Fk3XleikVjZiMPyTt5lCOAUQCwrbv0HX1fDgJsmHYkfJPRK8kRSOF00JGovG410'
   PUBLIC_KEY:'pk_test_51SIt9CJDRdvrnoGE1i7V1uIAllKH7k2IcrfgNVUJogzXaJRRDoFDHOdRUThOxxLmK0EX8iGOCx7aBcXkWJ1UwsVJ00wqAJWmKy'
