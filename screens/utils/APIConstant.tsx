1efec990d4f8d7430f1a5739f190a51641de19dd
saurabh <saurabhk@incrustsoftware.com>
2025-12-22 12:09:32 +0530

    stripe test key enabled

--- screens/utils/APIConstant.tsx
+++ screens/utils/APIConstant.tsx
@@ -1,11 +1,5 @@
 const MAIN_URL = {
   
-    // baseUrl: 'http://65.0.99.229:4320/',
-
-    // baseUrl: 'http://192.168.0.172:4320/',
-
-    // baseUrl:'http://18.175.150.246:4320/',
-
     // baseUrl:'http://13.134.87.43:4320/'
 
     baseUrl:"https://api.unizy.app/",
