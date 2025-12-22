a309b803e03f96b0b2fb44cb019302c1eb262803
borsemangesh <m.borse@incrustsoftware.com>
2025-12-22 12:19:12 +0530

    OnBoarding Screen Unable to Scroll the data

--- screens/view/onboarding/OnboardingScreen.tsx
+++ screens/view/onboarding/OnboardingScreen.tsx
@@ -7,14 +7,14 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 import OnboardingItem from './OnboardingItem';
 import Pagination from './Pagination';
 import { OnboardingData } from './OnboardingData';
 import { useTranslation } from 'react-i18next';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 const { width } = Dimensions.get('window');
 
 type OnBoardingProps = {
@@ -25,7 +25,7 @@ const OnboardingScreen = ({ navigation }: OnBoardingProps) => {
   const [index, setIndex] = useState(0);
   const ref = useRef<FlatList>(null);
   const { t } = useTranslation();
-  const handleNext = async() => {
+  const handleNext = async () => {
     if (index < OnboardingData.length - 1) {
       ref.current?.scrollToOffset({
         offset: (index + 1) * width,
@@ -36,18 +36,17 @@ const OnboardingScreen = ({ navigation }: OnBoardingProps) => {
       navigation.replace('Dashboard', {
         AddScreenBackactiveTab: 'Home',
         isNavigate: false,
-      })
+      });
     }
   };
 
   const handleSkip = async () => {
- 
     await AsyncStorage.setItem('ISONBOARDING', 'true');
 
     navigation.replace('Dashboard', {
       AddScreenBackactiveTab: 'Home',
       isNavigate: false,
-    })
+    });
   };
 
   const isFirst = index === 0;
@@ -60,6 +59,11 @@ const OnboardingScreen = ({ navigation }: OnBoardingProps) => {
       resizeMode="cover"
     >
       <View style={styles.container}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 60 }}
+        >
         <FlatList
           ref={ref}
           data={OnboardingData}
@@ -73,43 +77,45 @@ const OnboardingScreen = ({ navigation }: OnBoardingProps) => {
           }}
         />
 
+      </ScrollView>
         <Pagination data={OnboardingData} index={index} />
 
         <View style={styles.bottomRow}>
-          
-          
-
           {/* Next / Finish button */}
           {isLast ? (
             <>
-
-          <TouchableOpacity style={styles.finishbutton} onPress={handleNext}>
-            <Text style={styles.buttonText}>
-              {t('info_finish')}
-            </Text>
-          </TouchableOpacity>
-
+              <TouchableOpacity
+                style={styles.finishbutton}
+                onPress={handleNext}
+              >
+                <Text style={styles.buttonText}>{t('info_finish')}</Text>
+              </TouchableOpacity>
             </>
-          ):(<>
-          {!isLast && (
-            <TouchableOpacity style={styles.buttonskip} onPress={handleSkip}>
-              <Text style={styles.skip}>{t('info_skip')}</Text>
-            </TouchableOpacity>
-          )}
-             <TouchableOpacity style={styles.button} onPress={handleNext}>
-            <Text style={styles.buttonText}>
-              {isLast ? t('info_finish') : t('info_next')}
-            </Text>
+          ) : (
+            <>
+              {!isLast && (
+                <TouchableOpacity
+                  style={styles.buttonskip}
+                  onPress={handleSkip}
+                >
+                  <Text style={styles.skip}>{t('info_skip')}</Text>
+                </TouchableOpacity>
+              )}
+              <TouchableOpacity style={styles.button} onPress={handleNext}>
+                <Text style={styles.buttonText}>
+                  {isLast ? t('info_finish') : t('info_next')}
+                </Text>
 
-            {/* Arrow only for Next */}
-            {!isLast && (
-              <Image
-                source={require('../../../assets/images/arrowrightsm.png')}
-                style={{ width: 20, height: 20, marginLeft: 8 }}
-              />
-            )}
-          </TouchableOpacity>
-          </>)}
+                {/* Arrow only for Next */}
+                {!isLast && (
+                  <Image
+                    source={require('../../../assets/images/arrowrightsm.png')}
+                    style={{ width: 20, height: 20, marginLeft: 8 }}
+                  />
+                )}
+              </TouchableOpacity>
+            </>
+          )}
         </View>
       </View>
     </ImageBackground>
@@ -120,27 +126,24 @@ export default OnboardingScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
   bottomRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 16,
     alignItems: 'center',
-    bottom:30
-
+    bottom: 30,
   },
   clickskip: {
-    bottom: 50
+    bottom: 50,
   },
   skip: {
     color: '#FFFFFF',
     opacity: 0.7,
     fontFamily: 'Urbanist-Medium',
     fontSize: 14,
-    letterSpacing:1,
+    letterSpacing: 1,
     fontWeight: '600',
-    
   },
   button: {
     backgroundColor: ' rgba(255, 255, 255, 0.56)',
@@ -150,9 +153,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    alignContent  :'center',
-    justifyContent:'center',
-    gap: 4
+    alignContent: 'center',
+    justifyContent: 'center',
+    gap: 4,
   },
   finishbutton: {
     backgroundColor: ' rgba(255, 255, 255, 0.56)',
@@ -162,9 +165,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    alignContent  :'center',
-    justifyContent:'center',
-    gap: 4
+    alignContent: 'center',
+    justifyContent: 'center',
+    gap: 4,
   },
   buttonskip: {
     paddingVertical: 12,
@@ -173,13 +176,13 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    alignContent  :'center',
-    justifyContent:'center'
+    alignContent: 'center',
+    justifyContent: 'center',
   },
   buttonText: {
     fontWeight: '600',
     fontFamily: 'Urbanist-Medium',
     fontSize: 17,
-    letterSpacing:1,
+    letterSpacing: 1,
   },
 });
