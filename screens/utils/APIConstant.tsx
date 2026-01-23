59d6f5e4e1477b20962670ae72ad3c9fc4e37a6e
borsemangesh <m.borse@incrustsoftware.com>
2026-01-23 16:08:47 +0530

    Display the 2 time Popup Message In Code change

--- screens/utils/APIConstant.tsx
+++ screens/utils/APIConstant.tsx
@@ -1,6 +1,7 @@
 const MAIN_URL = {
   
     baseUrl:'http://13.233.238.97:4320/'
+    //Make it changes on Infor.plist file when dev mode is on (api.unizy.app)
 
     // baseUrl:"https://api.unizy.app/",
       
