c2bcc4de841f83021036ef97a80284c14879e179
borsemangesh <m.borse@incrustsoftware.com>
2026-02-04 11:40:35 +0530

    .

--- screens/utils/APIConstant.tsx
+++ screens/utils/APIConstant.tsx
@@ -1,9 +1,9 @@
 const MAIN_URL = {
   
-    baseUrl:'http://13.233.238.97:4320/'
+    // baseUrl:'http://13.233.238.97:4320/'
     //Make it changes on Infor.plist file when dev mode is on (api.unizy.app)
 
-    // baseUrl:"https://api.unizy.app/",
+    baseUrl:"https://api.unizy.app/",
       
     };
 
