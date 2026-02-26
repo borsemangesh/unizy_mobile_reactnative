658de153d4f9a44d5bb9357ddcb4cae28b96837a
saurabh <saurabhk@incrustsoftware.com>
2026-02-26 11:52:43 +0530

    accomodation changes

--- screens/view/dashboard/PreviewDetailed.tsx
+++ screens/view/dashboard/PreviewDetailed.tsx
@@ -319,7 +319,7 @@ const PreviewDetailed = ({ navigation }: previewDetailsProps) => {
     const isFeatured =
       form?.['13']?.value === true || form?.['13']?.value === 'true';
 
-    if (categoryid === 4) {
+    if (categoryid === 4 && accomodation_amount > 0) {
       navigation.navigate('PaymentScreen', {
         amount: accomodation_amount,
         feature_id: 1,
@@ -1107,7 +1107,7 @@ const PreviewDetailed = ({ navigation }: previewDetailsProps) => {
             try {
               const form = typeof storedForm === 'string' ? JSON.parse(storedForm) : storedForm;
               const isFeatured = form?.["13"]?.value === true || form?.["13"]?.value === 'true';
-              if (categoryid === Number(4)) {
+              if (categoryid === Number(4) && accomodation_amount > 0) {
                 return `${t('list')} for £${accomodation_amount.toFixed(2)}`;
               }
               return t('list');
