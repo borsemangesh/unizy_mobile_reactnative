d99fcf0a6cb1f6543ec509ad076af36bac5307fc
borsemangesh <m.borse@incrustsoftware.com>
2026-04-24 20:09:43 +0530

    .

--- screens/utils/component/FilterAndroid.tsx
+++ screens/utils/component/FilterAndroid.tsx
@@ -679,7 +679,7 @@ const FilterAndroid = ({
 
             <MultiSlider
               sliderLength={SCREEN_WIDTH / 2 - 10}
-              min={1}
+              min={0}
               max={isKm ? 1000 : kmToMiles(1000)}
               step={1}
               // values={[
